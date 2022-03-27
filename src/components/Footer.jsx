9425e2ee3e4d0c344bffd5a7b71c60be46bf7af9
suresh kumar <suresh475330@gmail.com>
2022-03-27 18:40:49 +0530

    three commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export let Footer = ()=>{
     return(
         <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200">
-           <h1 >Google search clone, Create by suresh</h1>
+           <h1 >Google search clone Create by suresh</h1>
           <a href='https://www.instagram.com/suresh__sk__07' target='_blank' rel='noreferrer' className='hover:underline'><h2 className="shadow hover:shadow-lg ">Instagram Id : <i class="fa-brands fa-instagram">  suresh__sk__07</i></h2></a>
           <i class="fa-brands fa-linkedin"></i>
         </div>
